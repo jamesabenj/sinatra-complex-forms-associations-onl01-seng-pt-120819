d7d364990ded12c75c05086b0ef5eee12752fb04
James Benjamin <james.abenj@gmail.com>
2020-03-30 14:29:13 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -16,7 +16,6 @@ class PetsController < ApplicationController
     pet = Pet.new(name: params[:pet_name], owner_id: params[:pet][:owner_id])
     owner = Owner.find(params[:pet][:owner_id])
     owner.pets << pet
-    end
     redirect to "/pets/#{pet.id}"
   end
 
