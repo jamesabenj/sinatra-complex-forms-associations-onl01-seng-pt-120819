405a0ecc35734551edd34cc90ed11324d3b3c452
James Benjamin <james.abenj@gmail.com>
2020-03-30 14:26:27 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,7 +15,7 @@ class PetsController < ApplicationController
     #binding.pry
     @pet = Pet.create(name: params[:pet_name],owner_id: params[:pet][:owner_id])
     binding.pry
-    owner = Owner.fing(params[:pet][:owner_id])
+    owner = Owner.find(params[:pet][:owner_id])
     @pet.owner << owner
     end
     redirect to "/pets/#{@pet.id}"
