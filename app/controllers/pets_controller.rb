0b5243b48cbc1626b6bbccad3df332a78807a0ff
James Benjamin <james.abenj@gmail.com>
2020-03-30 14:14:15 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -15,7 +15,7 @@ class PetsController < ApplicationController
     #binding.pry
     @pet = Pet.create(name: params[:pet_name],owner_id: params[:pet][:owner_id])
     if !params["pet"]["name"].empty?
-      @pet.owner << Pet.create(name: params["pet"]["name"])
+      @pet.owners << Pet.create(name: params["pet"]["name"])
     end
     redirect to "/pets/#{@pet.id}"
   end
